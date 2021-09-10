2a6e3dfae6c85b172e397b5ff8926c07db60cd6b
Wesley Guilherme Alves <wesley.guilherme@mercadolivre.com>
2021-09-10 19:20:30 -0300

    tt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,15 @@ import com.company.model.AnimaisDeFazenda;
 import com.company.model.AnimaisDomesticos;
 import com.company.model.Medico;
 import com.company.model.Proprietario;
+import org.json.simple.JSONObject;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.ParseException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -15,6 +21,30 @@ public class Main {
         Medico medico = new Medico("20747082030", "Etevaldo", "Lopes", 5555555L, "neuro");
         MedicoDAO medicoDAO = new MedicoDAO();
         medicoDAO.cadastrar(medico);
+
+
+        //Cria um Objeto JSON
+        JSONObject jsonObject = new JSONObject();
+
+        FileWriter writeFile = null;
+
+        //Armazena dados em um Objeto JSON
+        jsonObject.put("nome", "Allan");
+        jsonObject.put("sobrenome", "Romanato");
+        jsonObject.put("pais", "Brasil");
+        jsonObject.put("estado", "SP");
+
+        try {
+            writeFile = new FileWriter("saida.json");
+            //Escreve no arquivo conteudo do Objeto JSON
+            writeFile.write(jsonObject.toJSONString());
+            writeFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Imprimne na Tela o Objeto JSON para vizualização
+        System.out.println(jsonObject);
 //        System.out.println(medico.getCpf());
 //        Proprietario proprietario = new Proprietario("20747082030", "Godofredo", "Peppa", LocalDate.of(1998,07,23),"Rua dos Bobos, 0","56634639");
 //        AnimaisDeFazenda animaisDeFazenda = new AnimaisDeFazenda("Cavalo","Quarto de Milha","Marrom",LocalDate.of(2020,7,10),"Josefa",proprietario);
